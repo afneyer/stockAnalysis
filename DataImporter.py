cc5c481ee6ecb2f0161a9d5cb43f6b0d700b5f39
afneyer <afneyer@gmail.com>
2023-06-07 15:52:43 -0700

    test to correct dates

--- DataImporter.py
+++ DataImporter.py
@@ -16,14 +16,24 @@ class MyData:
     sp500_div_yield_month = 'SP500_Div_Yield_Month'
     sp500_real_price_month = 'SP500_Real_Price_Month'
     cpi_urban_month = 'CPI_Urban_Month'
+    ten_year_treasury = 'Ten_Year_Treasury'
 
     urls = [
         [sp500_pe_ratio_month, quandle, 'MULTPL/SP500_PE_RATIO_MONTH'],
         [sp500_div_yield_month, quandle, 'MULTPL/SP500_DIV_YIELD_MONTH'],
         [sp500_real_price_month, quandle, 'MULTPL/SP500_REAL_PRICE_MONTH'],
-        [cpi_urban_month, multpl, 'https://www.multpl.com/cpi/table/by-month']
+        [cpi_urban_month, multpl, 'https://www.multpl.com/cpi/table/by-month'],
+        [ten_year_treasury,multpl, 'https://www.multpl.com/10-year-treasury-rate/table/by-month']
     ]
 
+
+def correct_dates(df):
+    # drop end of the month date if there is already a date for the first of the next month
+    df = df.drop(df.loc[df['Date'].shift(1).eq(df['Date']+pd.DateOffset(days=-1))].index)
+    # move end of the month dates to the first
+    df['Date'] = df['Date'].apply(lambda x: x+pd.DateOffset(day=1))
+
+    return df
 
 class DataImporter:
 
@@ -58,12 +68,36 @@ class DataImporter:
         tr_elems = parsed.cssselect('#datatable tr')
         raw_data = [[td.text.strip() for td in tr_elem.cssselect('td')] for tr_elem in tr_elems[1:]]
 
-        df = pd.DataFrame(raw_data, columns=['Date', 'CPI_Monthly'])
+        df = pd.DataFrame(raw_data, columns=['Date', 'Value'])
         # parse date formats
         df.Date = pd.to_datetime(df.Date, format='%b %d, %Y')
+
+        # remove commas inside the values
+        df['Value'] = df['Value'].str.rstrip(',')
+
         # transform to numeric values
-        df['CPI_Monthly'] = pd.to_numeric(df['CPI_Monthly'])
-        # df.Price = pd.to_numeric(df.Price.str.replace(',', '').astype(float))  # handle commas inside strings
+        # check whether the series in percentage
+        if df['Value'].str.contains('%').any():
+            df['Value'] = df['Value'].str.rstrip('%').astype('float')/100.0
+        else:
+            df['Value'] = pd.to_numeric(df['Value'])
         df = df.set_index('Date')
 
         return df
+
+    def import_my_data(self):
+        for url in MyData.urls:
+            fi_name = url[0]
+            fi_type = url[1]
+            url_str = url[2]
+            if fi_type == 'quandle':
+                df = self.get_data_from_quandle(url_str)
+            elif fi_type == 'multpl':
+                 df = self.get_data_from_multpl_website(url_str)
+            df = df.rename(columns={'Value': fi_name})
+            df = apply_specific_data_fixes(df)
+            print(df.dtypes)
+            print(df.head)
+
+
+
--- test_DataImporter.py
+++ test_DataImporter.py
@@ -1,10 +1,10 @@
-import datetime
 from unittest import TestCase
 
+import numpy as np
 import pandas as pd
 from numpy.ma.testutils import approx
 
-from DataImporter import DataImporter, MyData
+from DataImporter import DataImporter, MyData, correct_dates
 
 
 class TestDataImporter(TestCase):
@@ -25,11 +25,10 @@ class TestDataImporter(TestCase):
         col_nam = MyData.sp500_div_yield_month
         df = df.rename(columns={'Value': col_nam})
         ts = pd.Timestamp('2022-8-31')
-        assert approx( df[MyData.sp500_div_yield_month][ts], 1.56)
+        assert approx(df[MyData.sp500_div_yield_month][ts], 1.56)
+        print(df.head())
 
     def test_get_data_from_multpl(self):
-        pe_url = 'http://www.multpl.com/table?f=m'
-        price_url = 'http://www.multpl.com/s-p-500-historical-prices/table/by-month'
         cpi_url = 'https://www.multpl.com/cpi/table/by-month'
 
         di = DataImporter()
@@ -37,3 +36,26 @@ class TestDataImporter(TestCase):
 
         print(cpi_data.head())
         # write tests
+
+    def test_import_my_data(self):
+        di = DataImporter()
+        di.import_my_data()
+        self.fail()
+
+    def test_correct_dates(self):
+        rng1 = pd.date_range('2015-01-31', periods=6, freq='M')
+        rng2 = pd.date_range('2015-04-01', periods=6, freq='MS')
+        rng = rng1.union(rng2)
+        df = pd.DataFrame({'Date': rng, 'Value': np.arange(0.0, len(rng), 1.0)})
+
+        df = correct_dates(df)
+        df = df.set_index(df['Date'])
+        # check length
+        assert len(df) == 8
+
+        # check that every date is a start month date
+        assert df['Date'].apply(lambda x: x.is_month_start).all()
+
+        # check a couple of values
+        assert approx(df.loc['2015-08-01'], 10.0)
+        assert approx(df.loc['2015-01-01'], 0.0)
