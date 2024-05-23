f5832e170924d048c7c2ace051e4340dbaf2d9b0
afn <afneyer@gmail.com>
2024-05-22 19:44:28 -0700

    fix warnings and errors in TestMonthlyData.py

--- TestBasics.py
+++ TestBasics.py
@@ -157,3 +157,24 @@ class MyTestCase(unittest.TestCase):
         s = pd.Series([1, np.nan, 3.0], dtype="float64")
         s1 = pd.to_numeric(s, downcast="float")
         assert s1.dtype == 'float32'
+
+    def test_date_ranges_for_plotting(self):
+        import matplotlib.pyplot as plt
+        import matplotlib.dates as mdates
+        import numpy as np
+        import datetime as dt
+
+        np.random.seed(1)
+
+        N = 100
+        y = np.random.rand(N)
+
+        now = dt.datetime.now()
+        then = now + dt.timedelta(days=100)
+        days = mdates.drange(now, then, dt.timedelta(days=1))
+
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
+        plt.plot(days, y)
+        plt.gcf().autofmt_xdate()
+        plt.show()
